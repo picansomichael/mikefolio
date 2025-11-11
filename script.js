514555fff687fadc6c7f3cc68837c0a64dbab405
picansomichael <picansomichael@gmail.com>
2025-11-11 07:36:25 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple JavaScript interaction
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("clickMe");
+  button.addEventListener("click", () => {
+    alert("Hello! Thanks for visiting my site.");
+  });
+});
